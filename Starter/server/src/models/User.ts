ffec9e65f96e9ebc657256d61d7d521bbbdde197
tomwalla917 <tywalla@gmail.com>
2026-02-02 18:36:54 -0500

    created user model in the src/model/user.ts file

--- Starter/server/src/models/User.ts
+++ Starter/server/src/models/User.ts
@@ -5,29 +5,89 @@ import sequelize from '../config/database';
 // TODO: Define the User attributes interface
 // Hint: User should have id, username, email, password, createdAt, updatedAt
 interface UserAttributes {
-  // TODO: Add properties here
+  id: number;
+  username: string;
+  email: string;
+  password: string;
+  createdAt?: Date;
+  updatedAt?:Date;
 }
 
 // TODO: Define optional attributes for creation (id, timestamps are auto-generated)
 interface UserCreationAttributes extends Optional<UserAttributes, 'id'> {}
 
+process.env.BCRYPT_SALT_ROUNDS || '10'
+
 // TODO: Create the User class extending Model
 class User extends Model<UserAttributes, UserCreationAttributes> implements UserAttributes {
-  // TODO: Declare public properties here
-  // Example: public id!: number;
+  public id!: number;
+  public username!: string;
+  public email!: string;
+  public password!: string;
+  public readonly createdAt?: Date;
+  public readonly updatedAt?:Date;
 }
 
 // TODO: Initialize the User model
 User.init(
   {
-    // TODO: Define your model attributes here
-    // Remember:
-    // - id should be primaryKey and autoIncrement
-    // - username should be unique and not null
-    // - email should be unique, not null, and validated as email
-    // - password should be not null with minimum length
+    id: {
+      type: DataTypes.INTEGER, 
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING(50), 
+      allowNull: false, 
+      validate: {
+        notNull: {
+          msg: 'Username is required'
+        },
+        notEmpty: {
+          msg: 'Username is required'
+        },
+        len: {
+          args: [3,50], 
+          msg: 'Username must be between 3 and 50 characters'
+        },
+      }
+    },
+    email: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Username is required'
+        },
+        notEmpty: {
+          msg: 'Username cannot be empty'
+        },
+        len: {
+          args: [3, 20],
+          msg: 'Username must be between 3 and 20 characters'
+        },
+      }
+    },
+    password: {
+      type: DataTypes.STRING(255), 
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Password is required'
+        },
+        notEmpty: {
+          msg: 'Password cannot be empty'
+        },
+        len: {
+          args: [3, 20],
+          msg: 'Password must be between 3 and 255 characters'
+        },
+      }
+    }
+
   },
   {
+    
     sequelize,
     modelName: 'User',
     tableName: 'users',
